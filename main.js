4a48955b7458c62b217099260d6a93ae512cc069
ianzote <ianfelipeevangelista11@gmail.com>
2025-09-06 19:41:14 -0300

    msg 'certo' e tempo de espera setTime

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 function validationForm(e) {
+    e.preventDefault();
+
     let campoA = document.querySelector('.A');
     let campoB = document.querySelector('.B');
 
     let valorA = Number(campoA.value) || 0;
     let valorB = Number(campoB.value) || 0;
 
-    const spanB = document.querySelector('.erro'); 
+    const spanB = document.querySelector('.erro');
+    const spanA = document.querySelector('.certo'); 
 
     if (valorB < valorA ) {
         e.preventDefault();
         spanB.innerText = 'O campo B deve ser maior que o campo A';
     } else {
-        spanB.innerText = '';
+        spanA.innerText = 'Formulário enviado com sucesso!';
+
+        setTimeout(() => {
+            spanA.innerText = '';
+            campoA.value = '';
+            campoB.value = '';
+        }, 1000);
     }
+    
 }
